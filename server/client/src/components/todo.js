88448d1d61b34d9ee0b85442664c13cb856c2331
Jonghan Park <jonghan.park@edu.sait.ca>
2022-08-21 18:01:58 -0600

    Update front end

---
+++ server/client/src/components/todo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const todo = () => {
+  return (
+    <div className="todoContainer">
+      <form className="todoForm">
+        <label className="todoLabel" htmlFor="todo">
+          To do:
+        </label>
+        <input className="todoInput" type="text" name="todo" />
+      </form>
+    </div>
+  );
+};
+
+export default todo;
