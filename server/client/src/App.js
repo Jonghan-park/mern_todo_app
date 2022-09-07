6b7ba344d53be2abd5c03deb75075009d9f97702
Jonghan Park <jonghan.park@edu.sait.ca>
2022-09-06 19:16:32 -0600

    Update login page

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Main from "./page/Main/Main";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Login />
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
