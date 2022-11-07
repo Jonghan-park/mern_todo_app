733282831e6c2e140bb221ad160c77d1fd48988b
Jonghan Park <jonghan.park@edu.sait.ca>
2022-11-07 16:10:00 -0700

    Update heroku

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const dbConnect = require("./db/db");
 const Todo = require("./model/todo");
 const todoRoute = require("./Routes/todo");
+const path = require("path");
 
 // Connect DB from ./db/db
 dbConnect();
@@ -13,6 +14,18 @@ dbConnect();
 app.use(cors());
 // express 4, has build-in body parser, I don't need to install body parser separately
 app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API running");
+  });
+}
 
 app.use("/api", todoRoute);
 app.listen(port, () => {
